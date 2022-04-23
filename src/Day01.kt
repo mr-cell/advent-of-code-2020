dbde8851a8c3465304bd1808c4607cbb1f4d52b3
mr-cell <marcel.schally@gmail.com>
2022-04-23 22:19:18 +0200

    Day1 naive approach

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val entries = input.map { it.toInt() }
+        for ((index, entry) in entries.withIndex()) {
+            for (candidate in entries.drop(index + 1)) {
+                if (entry + candidate == 2020) {
+                    return entry * candidate
+                }
+            }
+        }
+        return 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val entries = input.map { it.toInt() }
+        for ((index, entry) in entries.withIndex()) {
+            val restEntries = entries.drop(index + 1)
+            for ((candidateIndex, candidate1) in restEntries.withIndex()) {
+                for (candidate2 in restEntries.drop(candidateIndex + 1)) {
+                    if (entry + candidate1 + candidate2 == 2020) {
+                        return entry * candidate1 * candidate2
+                    }
+                }
+            }
+        }
+        return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val result = part1(testInput)
+    println(result)
+    println("=========")
+    check(result == 514579)
 
     val input = readInput("Day01")
     println(part1(input))
